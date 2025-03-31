9c6788571813c27f362f99d5338d18674a1b8c79
Jon Gonzalez <jon.gonzalez@mo2o.com>
2025-04-01 01:10:42 +0200

    feat: Add option to send custom decoder and encoder to JSONLoader

--- Sources/CompoKit/JSONLoader.swift
+++ Sources/CompoKit/JSONLoader.swift
@@ -10,13 +10,13 @@ import Foundation
 public protocol JSONLoader {}
 
 extension JSONLoader {
-    public func load<T>(url: URL, type: T.Type) throws -> T where T: Codable {
+    public func load<T>(url: URL, type: T.Type, decoder: JSONDecoder = JSONDecoder()) throws -> T where T: Codable {
         let data = try Data(contentsOf: url)
-        return try JSONDecoder().decode(type, from: data)
+        return try decoder.decode(type, from: data)
     }
 
-    public func save<T>(url: URL, data: T) throws where T: Codable {
-        let jsonData = try JSONEncoder().encode(data)
+    public func save<T>(url: URL, data: T, encoder: JSONEncoder = JSONEncoder()) throws where T: Codable {
+        let jsonData = try encoder.encode(data)
         try jsonData.write(to: url, options: [.atomic, .completeFileProtection])
     }
 }
